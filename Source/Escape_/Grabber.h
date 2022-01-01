16a2cdf6b0cf8e5d8443101df30e2a1013ae5f52
fviramontes8 <kuolemajarvi@yahoo.com>
2022-01-01 05:11:49 -0700

    Update Grabber.h
    Did a small refactor for Grabber.h as I reviewed some code. I merged two
    sections of the class declaration that is public into one because it was
    uncessary to have them separated.

--- Source/Escape_/Grabber.h
+++ Source/Escape_/Grabber.h
@@ -18,21 +18,18 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void FindPhysicsComponent();
 	void FindInputComponent();
 
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
 private:
 	float Reach = 200.0f;
-	
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
-
 	UInputComponent* InputComponent = nullptr;
 
 	void Grab();
